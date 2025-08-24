533c6c3aecec29ac832e0513ea59831327259248
Sufiyan <106079582+Sufiyanweb31@users.noreply.github.com>
2025-08-24 09:14:18 +0500

    Update motor insurance certificate styles and logo
    Refines layout and typography in motor-insurance-certificate.tsx for improved appearance, including padding, font sizes, alignment, and section spacing. Adds new style classes for better formatting, updates the logo image to logo2.png, and enhances the certificate's content structure for clarity.

--- components/Documents/motor-insurance-certificate.tsx
+++ components/Documents/motor-insurance-certificate.tsx
@@ -12,7 +12,7 @@ import {
 const styles = StyleSheet.create({
   page: {
     flexDirection: "column",
-    padding: 20,
+    padding: "10 20",
     fontFamily: "Helvetica",
     fontSize: 10,
     lineHeight: 1.1,
@@ -25,35 +25,41 @@ const styles = StyleSheet.create({
   addressBlock: {
     borderWidth: 1,
     borderColor: "#000",
-    padding: 8,
+    padding: 12,
     width: "55%",
     fontWeight: "bold",
+    height: 75,
   },
   logo: {
     width: 120,
-    height: 100,
+    height: 90,
+    objectFit: 'contain'
   },
   note: {
     textAlign: "center",
-    fontSize: 9,
-    marginVertical: 5,
+    fontSize: 10,
+    marginVertical: 8,
   },
   dottedLine: {
     borderBottomWidth: 1,
     borderBottomColor: "#000",
-    borderBottomStyle: "dotted",
-    marginVertical: 5,
+    borderBottomStyle: "dashed",
+    marginVertical: 6,
   },
   title: {
-    fontSize: 14,
+    fontSize: 22,
     fontWeight: "bold",
     textAlign: "center",
-    marginVertical: 8,
+    // marginVertical: 8,
+    marginBottom: 20,
   },
   mainBox: {
     borderWidth: 1,
     borderColor: "#000",
-    padding: 8,
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 5,
+    paddingBottom: 5,
     marginBottom: 0,
   },
   row: {
@@ -69,6 +75,7 @@ const styles = StyleSheet.create({
   twoColRow: {
     flexDirection: "row",
     justifyContent: "space-between",
+    marginRight: 60,
   },
   bold: {
     fontWeight: "bold",
@@ -81,6 +88,7 @@ const styles = StyleSheet.create({
     borderColor: "#000",
     padding: 8,
     marginBottom: 0,
+    borderTopWidth: 0,
   },
   certRow: {
     flexDirection: "row",
@@ -88,25 +96,52 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   signatureBlock: {
-    alignItems: "flex-end",
+    alignItems: "flex-start",
+    marginRight: 50,
   },
   noteBox: {
     borderWidth: 1,
     borderColor: "#000",
     padding: 8,
     marginBottom: 10,
+    borderTopWidth: 0,
+    fontSize: 9,
+    lineHeight: 1.1
   },
   footer: {
     textAlign: "center",
     fontSize: 8,
-    marginTop: 5,
+    marginTop: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: 5,
+    width: "75%"
   },
   hotline: {
     textAlign: "center",
-    fontSize: 12,
+    fontSize: 15,
     fontWeight: "bold",
     marginTop: 5,
   },
+  innsurencefont:{
+    fontSize: 8,
+    marginTop: 10,
+  },
+  hereby:{
+     width: "60%"
+  },
+  certify:{
+    fontSize: 9,
+    marginBottom: 8,
+  },
+  authen:{
+    marginTop: 10,
+    fontSize: 10,
+  },
+  footernumber:{
+    textAlign: "right",
+    fontSize: 8,
+  }
 });
 
 const MotorInsuranceCertificate = () => (
@@ -120,7 +155,7 @@ const MotorInsuranceCertificate = () => (
           <Text>Dagenham, Greater London</Text>
           <Text>RM8 2RP</Text>
         </View>
-        <Image src="/logo.png" style={styles.logo} />
+        <Image src="/logo2.png" style={styles.logo} />
       </View>
 
       <Text style={styles.note}>
@@ -141,14 +176,14 @@ const MotorInsuranceCertificate = () => (
             <View style={styles.twoColRow}>
               <Text>a) Registration mark of vehicle.</Text>
               <Text>
-                Policy Number: <Text style={styles.bold}>TCV-MOT-44072540</Text>
+                Policy Number: <Text style={[styles.bold, styles.italic]}>TCV-MOT-44072540</Text>
               </Text>
             </View>
-            <Text style={styles.bold}>LF52BVB</Text>
+            <Text style={[styles.bold, styles.italic]}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; LF52BVB</Text>
           </View>
         </View>
         {/* 1b */}
-        <View style={[styles.row, { marginLeft: 15 }]}>
+        <View style={[styles.row, { marginLeft: 15, marginTop: 10, }]}>
           <Text style={styles.content}>
             b) Any vehicle supplied to the policyholder under an agreement
             between Ageas Insurance Limited and a repairer, whilst the vehicle
@@ -159,46 +194,46 @@ const MotorInsuranceCertificate = () => (
         </View>
 
         {/* 2 */}
-        <View style={styles.row}>
+        <View style={[styles.row]}>
           <Text style={styles.number}>2.</Text>
           <View style={styles.content}>
             <Text>Description of vehicle.</Text>
-            <Text style={styles.bold}>NISSAN MICRA TEMPEST</Text>
+            <Text style={[styles.bold, styles.italic]}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; NISSAN MICRA TEMPEST</Text>
           </View>
         </View>
 
         {/* 3 */}
-        <View style={styles.row}>
+        <View style={[styles.row, {marginTop: 8,}]}>
           <Text style={styles.number}>3.</Text>
           <View style={styles.content}>
             <Text>Name of policyholder.</Text>
-            <Text style={styles.bold}>Mr Uzzal Miah</Text>
+            <Text style={[styles.bold, styles.italic]}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mr Uzzal Miah</Text>
           </View>
         </View>
 
         {/* 4 */}
-        <View style={styles.row}>
+        <View style={[styles.row, {marginTop: 8,}]}>
           <Text style={styles.number}>4.</Text>
           <View style={styles.content}>
             <Text>
               Effective date of commencement of insurance for the purposes of
               the relevant law.
             </Text>
-            <Text style={styles.bold}>09 Jun 2025 15:35</Text>
+            <Text  style={[styles.bold, styles.italic]}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;09 Jun 2025 15:35</Text>
           </View>
         </View>
 
         {/* 5 */}
-        <View style={styles.row}>
+        <View style={[styles.row, {marginTop: 8,}]}>
           <Text style={styles.number}>5.</Text>
           <View style={styles.content}>
             <Text>Date of expiry of insurance.</Text>
-            <Text style={styles.bold}>09 Jun 2025 16:35</Text>
+            <Text  style={[styles.bold, styles.italic]}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;09 Jun 2025 16:35</Text>
           </View>
         </View>
 
         {/* 6 */}
-        <View style={styles.row}>
+        <View style={[styles.row, {marginTop: 8,}]}>
           <Text style={styles.number}>6.</Text>
           <View style={styles.content}>
             <Text>Persons or classes of persons entitled to drive.</Text>
@@ -207,40 +242,44 @@ const MotorInsuranceCertificate = () => (
               licence to drive the vehicle or has held and is not disqualified
               from holding or obtaining such a licence.
             </Text>
-            <Text style={styles.bold}>Mr Uzzal Miah</Text>
+            <Text  style={[styles.bold, styles.italic]}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mr Uzzal Miah</Text>
           </View>
         </View>
 
         {/* 7 */}
-        <View style={styles.row}>
+        <View style={[styles.row, {marginTop: 8,}]}>
           <Text style={styles.number}>7.</Text>
           <View style={styles.content}>
             <Text>Limitations as to use subject to the exclusions below.</Text>
-            <Text style={styles.bold}>
+            <Text style={[styles.bold, styles.italic]}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               Use for social Domestic and Pleasure purposes and use in person by
               the Policyholder in connection with their business or profession.
             </Text>
-            <Text style={{ marginTop: 5 }}>The insurance does not cover:</Text>
+            <View style={styles.innsurencefont}>
+              <Text style={{ marginTop: 5 }}>The insurance does not cover:</Text>
             <Text>• Use for racing, pacemaking, competition, rallies trials or speedtesting.</Text>
             <Text>
               • Use to secure the release of a motor vehicle, which has been
               seized by, or on behalf of, any Government or public authority.
             </Text>
+            </View>
           </View>
         </View>
       </View>
 
       {/* certification */}
       <View style={styles.certification}>
-        <Text>
-          I hereby certify that the policy to which this certificate relates
-          satisfies the requirements of the relevant law applicable in Great
-          Britain, Northern Ireland, the Isle of Man, the Island of Guernsey,
-          the Island of Jersey and the Island of Alderney
-        </Text>
-
         <View style={styles.certRow}>
-          <Text style={styles.bold}>TEMPCOVER LTD{"\n"}AUTHENTICATED as authorised agent on behalf of the Insurer</Text>
+          <View style={styles.hereby}>
+            <Text style={styles.certify}>
+                I hereby certify that the policy to which this certificate relates
+                satisfies the requirements of the relevant law applicable in Great
+                Britain, Northern Ireland, the Isle of Man, the Island of Guernsey,
+                the Island of Jersey and the Island of Alderney
+            </Text>
+            <Text style={[styles.bold, { textAlign: 'center'}]}>TEMPCOVER LTD</Text>
+            <Text style={[styles.authen]}>AUTHENTICATED as authorised agent on behalf of the Insurer</Text>
+          </View>
           <View style={styles.signatureBlock}>
             <Text>Ageas Insurance Limited</Text>
             <Text>Authorised Insurers</Text>
@@ -273,7 +312,7 @@ const MotorInsuranceCertificate = () => (
         Templers Way, Eastleigh, Hampshire, SO53 3YA Registered in England and
         Wales No 354568
       </Text>
-      <Text style={styles.footer}>127598.0.S</Text>
+      <Text style={styles.footernumber}>127598.0.S</Text>
       <Text style={styles.hotline}>Claims Hotline: 0333 241 3392</Text>
     </Page>
   </Document>
